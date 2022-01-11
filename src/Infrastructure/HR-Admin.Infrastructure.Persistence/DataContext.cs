caf0df73dc38e8e0306f86257959a310ff6cc48f
Miguel Jiménez <miguelp90.319@gmail.com>
2022-01-11 19:31:46 -0400

    DB Seeder & data context on persistence layer

---
+++ src/Infrastructure/HR-Admin.Infrastructure.Persistence/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using HR_Admin.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace HR_Admin.Infrastructure.Persistence
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options) {}
+
+        public DbSet<Department> Departments { get; set; }
+    }
+}
---
+++ src/Infrastructure/HR-Admin.Infrastructure.Persistence/Seeder/DbSeeder.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+
+namespace HR_Admin.Infrastructure.Persistence.Seeder
+{
+    public class DbSeeder
+    {
+        public static async Task SeedData(DataContext context)
+        {
+            if (context.Departments.Any()) return;
+
+            var departmentData = DepartmentData.GetDepartments();
+
+            await context.Departments.AddRangeAsync(departmentData);
+            await context.SaveChangesAsync();
+        }
+    }
+}
