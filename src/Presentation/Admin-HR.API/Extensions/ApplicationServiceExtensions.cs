34c3f2a6d9290e60ffb01e5ea318d9a9d6a0bfdf
Miguel Jiménez <miguelp90.319@gmail.com>
2022-01-13 00:15:25 -0400

    Fluent validation on dept. creation and update

---
+++ src/Presentation/Admin-HR.API/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,30 @@
+﻿using Admin_HR.Infrastructure.Persistence;
+using HR_Admin.Application.Core;
+using HR_Admin.Application.Departments;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+
+namespace Admin_HR.API.Extensions
+{
+    public static class ApplicationServiceExtensions
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services,
+            IConfiguration config)
+        {
+            services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new OpenApiInfo {Title = "WebAPIv5", Version = "v1"}); });
+
+            services.AddDbContext<DataContext>(op =>
+                op.UseSqlite(config.GetConnectionString(("DefaultConnection")))
+            );
+
+            services.AddMediatR(typeof(List.Handler).Assembly);
+            
+            services.AddAutoMapper(typeof(MappingProfiles).Assembly);
+
+            return services;
+        }
+    }
+}
