e7cadef7519c3c0d37cf8e5c2ad6724d8301ae85
Miguel Jiménez <miguelp90.319@gmail.com>
2022-01-12 21:23:05 -0400

    Department post request

---
+++ src/Infrastructure/Admin-HR.Infrastructure.Persistence/Data/DepartmentData.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using Admin_HR.Domain.Entities;
+
+namespace Admin_HR.Infrastructure.Persistence.Data
+{
+    class DepartmentData
+    {
+        public static List<Department> GetDepartments()
+            => new List<Department>()
+            {
+                new()
+                {
+                    Name = "Human Resource",
+                    Code = "HR-231",
+                    CreatedAt = DateTime.Now.AddDays(-5),
+                    UpdatedAt = DateTime.Now.AddDays(-5),
+                },
+                new()
+                {
+                    Name = "Marketing",
+                    Code = "Mk-356",
+                    CreatedAt = DateTime.Now.AddDays(-23),
+                    UpdatedAt = DateTime.Now.AddDays(-23)
+                },
+                new()
+                {
+                    Name = "Accounting and Finance",
+                    Code = "Acc-Fin-125",
+                    CreatedAt = DateTime.Now.AddDays(-2),
+                    UpdatedAt = DateTime.Now.AddDays(-2)
+                },
+                new()
+                {
+                    Name = "Operations",
+                    Code = "Ops-896",
+                    CreatedAt = DateTime.Now.AddDays(-35),
+                    UpdatedAt = DateTime.Now.AddDays(-35)
+                },
+                new()
+                {
+                    Name = "Sales",
+                    Code = "Sales-481",
+                    CreatedAt = DateTime.Now.AddHours(-14),
+                    UpdatedAt = DateTime.Now.AddHours(-14)
+                },
+                new()
+                {
+                    Name = "Customer Service",
+                    Code = "CS-782",
+                    CreatedAt = DateTime.Now,
+                    UpdatedAt = DateTime.Now
+                }
+            };
+    }
+}
---
+++ src/Infrastructure/Admin-HR.Infrastructure.Persistence/Data/Seeder/DbSeeder.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+
+namespace Admin_HR.Infrastructure.Persistence.Data.Seeder
+{
+    public class DbSeeder
+    {
+        public static async Task SeedData(DataContext context)
+        {
+            if (context.Departments.Any()) return;
+
+            var departmentData = DepartmentData.GetDepartments();
+
+            await context.Departments.AddRangeAsync(departmentData);
+            await context.SaveChangesAsync();
+        }
+    }
+}
