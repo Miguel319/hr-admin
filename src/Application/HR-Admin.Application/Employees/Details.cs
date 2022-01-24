ca0f36d88712201f75d1519ce458b8d3fb0670e7
Miguel Jiménez <miguelp90.319@gmail.com>
2022-01-24 11:27:28 -0400

    Employee list and details

---
+++ src/Application/HR-Admin.Application/Employees/Details.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Admin_HR.Domain.Entities;
+using Admin_HR.Infrastructure.Persistence;
+using HR_Admin.Application.Core;
+using MediatR;
+
+namespace HR_Admin.Application.Employees
+{
+    public class Details
+    {
+        public class Query : IRequest<Result<Employee>>
+        {
+            public Guid Id { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Query, Result< Employee>>
+        {
+            private readonly DataContext _context;
+
+            public Handler(DataContext context)
+                => _context = context;
+
+            public async Task<Result<Employee>?> Handle(Query request, CancellationToken cancellationToken)
+            {
+                var employee = await _context.Employees.FindAsync(new object?[] { request.Id }, cancellationToken);
+
+                if (employee == null) return null;
+              
+                return Result<Employee>.Success(employee);
+            } 
+        } 
+    }
+}
---
+++ src/Application/HR-Admin.Application/Employees/List.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Admin_HR.Domain.Entities;
+using Admin_HR.Infrastructure.Persistence;
+using HR_Admin.Application.Core;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace HR_Admin.Application.Employees
+{
+    public class List
+    {
+        public class Query : IRequest<Result<List<Employee>>>
+        {
+            
+        }
+
+        public class Handler : IRequestHandler<Query, Result<List<Employee>>>
+        {
+            private readonly DataContext _context;
+
+            public Handler(DataContext context) => _context = context;
+
+            public async Task<Result<List<Employee>>> Handle(Query request, CancellationToken cancellationToken)
+            {
+                var employees = await _context.Employees.ToListAsync(cancellationToken);
+
+                return Result<List<Employee>>.Success(employees);
+            }
+        }
+    }
+}
