c58aa421fe22a0fd7e5b2b49a41c56112ff72715
Miguel Jiménez <miguelp90.319@gmail.com>
2022-01-23 22:48:30 -0400

    Position entity

---
+++ src/Domain/Admin-HR.Domain/Entities/Position.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Admin_HR.Domain.Entities
+{
+    public class Position
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public double MinSalary { get; set; }
+        public double MaxSalary { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
